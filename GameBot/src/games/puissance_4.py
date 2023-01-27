1661b25b0bd11040804a26d539f42da45dabb17f
Sylvaindelaforet <74321124+Sylvaindelaforet@users.noreply.github.com>
2023-01-27 14:36:09 +0100

    màj puissance 4, read command

--- GameBot/src/games/puissance_4.py
+++ GameBot/src/games/puissance_4.py
@@ -27,49 +27,55 @@ class Grille:
 
     # vérification que la colonne est non pleine
     def choixPossible(self, choix):
-        return (self.grille[int(choix)][-1] == CouleurCase.Vide) & (int(choix) in [0,1,2,3,4,5,6])
+        return (self.grille[int(choix)][-1] == CouleurCase.Vide) and (int(choix) in [0,1,2,3,4,5,6])
     
     # ajout tuile colonne
-    def ajouter(self, joueur, colonne):
+    def ajouter(self, joueur: int, colonne: int) -> tuple:
+        # check
+        if CouleurCase(joueur) == self.derniereCouleur:
+            raise Exception("Puissance_4 : Same player can't play twice in a row")
+        if colonne not in range(Grille.col):
+            raise Exception("Puissance_4 : Column ", colonne, "out of range")
+        
         for ligne in range(Grille.row - 1, -1, -1):
             if self.grille[colonne][ligne - 1] != CouleurCase.Vide:
                 self.grille[colonne][ligne] = CouleurCase(joueur)
                 self.derniereCouleur = CouleurCase(joueur)
                 self.checkFini(colonne,ligne)
+                return (colonne, ligne)
 
 
     # arg : colonne = colonne du dernier placement;; ligne = ligne du dernier placement
-    def checkFinie (self, colonne, ligne):
+    def checkFini (self, colonne: int, ligne:int) -> None:
         alignementVert = self.parcourir(1, 0, colonne, ligne) + self.parcourir(-1, 0, colonne, ligne)
         alignementHoriz = self.parcourir(0, 1, colonne, ligne) + self.parcourir(0, -1, colonne, ligne)
         # diagonale : bas à gauche vers haut à droite 
         alignementDiagX = self.parcourir(1, 1, colonne, ligne) + self.parcourir(-1, -1, colonne, ligne)
-        # diagonale
+        # diagonale : descendant
         alignementDiag_X = self.parcourir(-1, 1, colonne, ligne) + self.parcourir(1, -1, colonne, ligne)
         if alignementVert >= 3 or alignementHoriz >= 3 or alignementDiagX >= 3 or alignementDiag_X >= 3:
              self.finie = True
 
 
     # dligne = dx et dcolonne = dy
-    def parcourir(self, dcolonne, dligne, colonne, ligne):
+    def parcourir(self, dcolonne:int, dligne:int, colonne:int, ligne:int) -> int:
         # check still in grid
         if (colonne + dcolonne in range(Grille.col)) & (ligne + dligne in range(Grille.row)):
             if self.grille[colonne + dcolonne][ligne + dligne] == self.derniereCouleur :
                 return 1 + self.parcourir(dcolonne, dligne, colonne + dcolonne, ligne + dligne)
         return 0
 
-
     # getter
-    def isFinished(self, dernierPlacement):
+    def isFinished(self) -> bool:
         return self.finie
     
-    def reset(self):
+    def reset(self) -> None:
         for i in range(Grille.col):
             for j in range(Grille.row):
                 self.grille[i] = CouleurCase.Vide
 
-    # affiche la grille
-    def afficher(self):
+    # affiche la grille en commande
+    def afficher(self) -> None:
         for i in reversed(range(6)):
             for j in range(7):
                 print(end='|| ')
@@ -83,13 +89,20 @@ class GamePuissance4(Game):
     def __init__(self):
         self.grille = Grille()
 
+
+    # formats commandes : {"joueur":int (1 ou 2), "colonne":int (0 - 6)}
     def command(self, command: str) -> None:
-        
-        raise Exception("Game method command() is not implemented !")
-    
+        joueur = command.get("joueur", -1)
+        colonne = command.get("colonne", -1)
+        if joueur == -1 or colonne == -1:
+            raise Exception("Puissance_4 : missing value in command :", command)
+        pos = self.grille.ajouter(joueur, colonne)
+
+
     def finished(self) -> bool:
         self.grille.isFinished()
 
     def reset(self) -> None:
         self.grille.reset()
+
 
